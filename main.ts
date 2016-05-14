0e12b7029abb8b4232ad2d4620f1b881846d7727
Peter Safranek <pe8ter@mac.com>
2016-05-14 11:26:49 -0700

    Convert to TypeScript and add stub implementation.

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+import { Socket } from 'net';
+
+const MIN_REGISTERD_PORT = 1024;
+const MAX_REGISTERD_PORT = 49151;
+const MIN_INPUT = 1000;
+const MAX_INPUT = 49999;
+
+export function randPort(start?: number): Promise<number> {
+
+    return Promise.resolve(0);
+}
